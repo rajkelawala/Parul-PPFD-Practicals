8c12d241549948f640ee988faf5cfc81c4117765
Raj kelawala <raj@jetbro.in>
2026-01-21 15:33:27 +0530

    Second_Commit

---
+++ Set-3/p1/server.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+HOST = "localhost"
+PORT = 8000
+
+class StaticServer(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'  # Route root path to index.html
+        return super().do_GET()
+    
+def run_server():
+    with HTTPServer((HOST, PORT), StaticServer) as server:
+        print(f"Serving on http:// {HOST}:{PORT}")
+        server.serve_forever()
+
+if __name__ == "__main__":
+    run_server()
