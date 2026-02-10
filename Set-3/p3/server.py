7d079e97b0814cd5d500089c28d56873cccc587e
Raj kelawala <raj@jetbro.in>
2026-02-10 09:41:11 +0530

    Next_Commit

--- Set-3/p3/server.py
+++ Set-3/p3/server.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #16MB
 
 @app.route('/')
 def home():
--- Set-3/p5/server.py
+++ Set-3/p5/server.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-# Route for the home page with the input form
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # Get user input from the form
         user_input = request.form["user_input"]
-        # Process the input (in this case, reverse the string)
         processed_data = user_input[::-1]
-        # Pass the processed data to the result template
         return render_template("result.html", original=user_input,                           result=processed_data)
-    # Render the form on GET request
     return render_template("input.html")
 
 if __name__ == "__main__":
